8f68364cb5679aea0bd68a02a7f0ee6a49cf4e12
hanseounggyun <hanseu9839@gmail.com>
2025-01-10 00:05:44 +0900

    fix: ExceptionResponse SnakeCase 추가

--- src/main/java/com/realworld/common/response/ExceptionResponse.java
+++ src/main/java/com/realworld/common/response/ExceptionResponse.java
@@ -8,6 +8,7 @@ import org.springframework.http.HttpStatus;
 @Getter
 @Builder
 @RequiredArgsConstructor
+@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
 public class ExceptionResponse<T> {
     private final HttpStatus httpStatus;
     private final int resultCode;

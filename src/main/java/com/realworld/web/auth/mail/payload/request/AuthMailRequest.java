e5427ebd856acb309a7a3b110797e1093c9b2490
hanseounggyun <hanseu9839@gmail.com>
2025-01-13 00:52:39 +0900

    🚑 hotfix: record request @Getter 제거

--- src/main/java/com/realworld/web/auth/mail/payload/request/AuthMailRequest.java
+++ src/main/java/com/realworld/web/auth/mail/payload/request/AuthMailRequest.java
@@ -5,9 +5,7 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
-import lombok.Getter;
 
-@Getter
 @Schema(description = "인증 메일 요청 Request")
 public record AuthMailRequest(
         @JsonProperty("user_email") @Schema(description = "유저 메일", example = "test@naver.com")

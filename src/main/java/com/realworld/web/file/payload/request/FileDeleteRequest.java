f9d535573457e4c89552d388415d956d9defc67c
CodeJesterND <dev.hyoseung@gmail.com>
2025-01-13 01:53:19 +0900

    📝 문서 추가 : FileDeleteRequest DTO에 Swagger 어노테이션 및 유효성 검증 추가 - FileDeleteRequest 클래스에 @Schema 어노테이션 추가하여 Swagger 문서화 개선 - `urls` 필드에 @Schema 어노테이션 추가하여 필드 설명 및 예시 제공 - `urls` 필드에 @NotEmpty 어노테이션 추가하여 최소 하나 이상의 URL 필수 입력 - `urls` 리스트를 불변 리스트로 설정하여 데이터 무결성 강화
    Ref : #18

--- src/main/java/com/realworld/web/file/payload/request/FileDeleteRequest.java
+++ src/main/java/com/realworld/web/file/payload/request/FileDeleteRequest.java
@@ -3,15 +3,25 @@ package com.realworld.web.file.payload.request;
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.databind.PropertyNamingStrategies;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotEmpty;
 import lombok.Getter;
 
 import java.util.Collections;
 import java.util.List;
 
+@Schema(
+        description = "파일 삭제 요청 DTO"
+)
 @Getter
 @JsonNaming(value = PropertyNamingStrategies.SnakeCaseStrategy.class)
 public class FileDeleteRequest {
 
+    @Schema(
+            description = "삭제할 파일의 URL 목록",
+            example = "['http://example.com/file1.jpg', 'http://example.com/file2.png']"
+    )
+    @NotEmpty(message = "urls는 필수 입력값이며, 최소 하나 이상의 URL을 포함해야 합니다.")
     private final List<String> urls;
 
     @JsonCreator

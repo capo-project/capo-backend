cfdb9461ba10e74ff4b3f2d5fa602a2a923db0f5
CodeJesterND <dev.hyoseung@gmail.com>
2025-01-13 01:22:46 +0900

    ✨ 새 기능 : 파일 관리 API에 Swagger 문서화 추가 및 설명 수정 - 파일 관리 컨트롤러에 Swagger @Operation 어노테이션 추가 - 각 API 엔드포인트에 대한 요약 및 상세 설명을 한글로 작성 - 성공 및 오류 응답 메시지를 한글로 일관되게 수정 - Swagger @Tag 어노테이션을 통해 파일 관리 API 그룹화 - API 응답 코드 및 설명을 명확히 정의하여 문서화 개선
    Ref : #18

---
+++ src/main/java/com/realworld/web/file/controller/FileImageController.java
@@ -0,0 +1,124 @@
+package com.realworld.web.file.controller;
+
+import com.realworld.application.file.service.FileImageService;
+import com.realworld.common.response.SuccessResponse;
+import com.realworld.common.response.code.ExceptionResponseCode;
+import com.realworld.common.response.code.SuccessResponseCode;
+import com.realworld.common.swagger.ExceptionResponseAnnotations;
+import com.realworld.common.swagger.SuccessResponseAnnotation;
+import com.realworld.web.file.payload.request.FileDeleteRequest;
+import com.realworld.web.file.payload.request.FileMoveRequest;
+import com.realworld.web.file.payload.request.FileUploadRequest;
+import com.realworld.web.file.payload.response.FileResponse;
+import com.realworld.web.file.payload.response.FileResponses;
+import com.realworld.web.file.payload.response.FileUrlResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Arrays;
+import java.util.List;
+
+@Tag(
+        name = "파일 관리",
+        description = "이미지 파일 업로드, 리사이즈, 이동 및 삭제를 위한 API"
+)
+@RestController
+@RequestMapping("/files/images")
+@RequiredArgsConstructor
+public class FileImageController {
+
+    private final FileImageService fileImageService;
+
+    @SuccessResponseAnnotation(SuccessResponseCode.SUCCESS)
+    @ExceptionResponseAnnotations({
+            ExceptionResponseCode.UNSUPPORTED_FILE_IMAGE_TYPE_ERROR,
+            ExceptionResponseCode.FILE_PROCESSING_ERROR,
+            ExceptionResponseCode.FILE_IMAGE_RESIZE_ERROR
+    })
+    @PostMapping(
+            value = "/resize",
+            consumes = MediaType.MULTIPART_FORM_DATA_VALUE
+    )
+    public ResponseEntity<SuccessResponse<FileResponses>> uploadResizedImage(@ModelAttribute FileUploadRequest request) {
+        List<FileResponse> fileResponses = Arrays.stream(request.getMultipartFiles())
+                .map(file -> FileResponse.from(
+                        fileImageService.saveResizedImage(
+                                request.getDestinationDirectory(),
+                                file,
+                                request.getWidth(),
+                                request.getHeight()
+                        )
+                ))
+                .toList();
+
+        FileResponses responses = FileResponses.of(fileResponses);
+
+        SuccessResponse<FileResponses> successResponse = new SuccessResponse<>(
+                responses,
+                200,
+                HttpStatus.OK,
+                "파일 업로드 성공"
+        );
+
+        return ResponseEntity.ok(successResponse);
+    }
+
+    @SuccessResponseAnnotation(SuccessResponseCode.SUCCESS)
+    @ExceptionResponseAnnotations({
+            ExceptionResponseCode.UNSUPPORTED_FILE_IMAGE_TYPE_ERROR,
+            ExceptionResponseCode.FILE_PROCESSING_ERROR,
+            ExceptionResponseCode.FILE_IMAGE_RESIZE_ERROR
+    })
+    @PostMapping(consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    public ResponseEntity<SuccessResponse<FileResponses>> upload(@ModelAttribute FileUploadRequest request) {
+        List<FileResponse> fileResponses = Arrays.stream(request.getMultipartFiles())
+                .map(file -> FileResponse.from(
+                        fileImageService.saveImage(
+                                request.getDestinationDirectory(),
+                                file
+                        )
+                ))
+                .toList();
+
+        FileResponses responses = FileResponses.of(fileResponses);
+
+        SuccessResponse<FileResponses> successResponse = new SuccessResponse<>(
+                responses,
+                200,
+                HttpStatus.OK,
+                "파일 업로드 성공"
+        );
+
+        return ResponseEntity.ok(successResponse);
+    }
+
+    @SuccessResponseAnnotation(SuccessResponseCode.SUCCESS)
+    @PatchMapping("/move")
+    public ResponseEntity<SuccessResponse<FileUrlResponses>> move(@RequestBody FileMoveRequest request) {
+        List<String> movedUrls = request.getUrls().stream()
+                .map(url -> fileImageService.move(url, request.getDirectory()))
+                .toList();
+
+        FileUrlResponses responses = FileUrlResponses.of(movedUrls);
+
+        SuccessResponse<FileUrlResponses> successResponse = new SuccessResponse<>(
+                responses,
+                200,
+                HttpStatus.OK,
+                "파일 이동 성공"
+        );
+
+        return ResponseEntity.ok(successResponse);
+    }
+
+    @DeleteMapping
+    public ResponseEntity<SuccessResponse<FileResponse>> delete(@RequestParam("files") FileDeleteRequest request) {
+        request.getUrls().forEach(fileImageService::delete);
+        return ResponseEntity.ok(new SuccessResponse<>(null, 200, HttpStatus.OK, "파일 삭제 성공"));
+    }
+
+}

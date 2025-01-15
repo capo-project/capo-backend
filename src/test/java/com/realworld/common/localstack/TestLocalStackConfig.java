5355885093a510e6a9d8bed5c9779ccc56e99f8d
CodeJesterND <dev.hyoseung@gmail.com>
2025-01-15 15:03:42 +0900

    ♻️ 코드 리팩토링 : 테스트 환경용 LocalStack 설정 추가 - TestLocalStackConfig 클래스 추가하여 LocalStack과 통합된 AmazonS3 클라이언트 구성 - 테스트 프로파일에서 사용할 S3 버킷 자동 생성 로직 구현 - AWS 자격증명 및 엔드포인트 설정을 통해 로컬 환경에서 S3 접근 가능하게 설정 - `@Primary` 어노테이션을 사용하여 테스트 시 기본 AmazonS3 빈으로 설정
    Ref : #21

---
+++ src/test/java/com/realworld/common/localstack/TestLocalStackConfig.java
@@ -0,0 +1,63 @@
+package com.realworld.common.localstack;
+
+import com.amazonaws.auth.AWSCredentials;
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.client.builder.AwsClientBuilder;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import jakarta.validation.constraints.NotNull;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Primary;
+import org.springframework.context.annotation.Profile;
+import org.springframework.validation.annotation.Validated;
+
+@Validated
+@Profile("test")
+@TestConfiguration
+public class TestLocalStackConfig {
+
+    private final String region;
+    private final String bucket;
+    private final String endpoint;
+    private final String accessKey;
+    private final String secretKey;
+
+    public TestLocalStackConfig(
+            @NotNull @Value("${localstack.region.static}") String region,
+            @NotNull @Value("${localstack.s3.bucket}") String bucket,
+            @NotNull @Value("${localstack.s3.endpoint}") String endpoint,
+            @NotNull @Value("${localstack.credentials.access-key}") String accessKey,
+            @NotNull @Value("${localstack.credentials.secret-key}") String secretKey
+    ) {
+        this.region = region;
+        this.bucket = bucket;
+        this.endpoint = endpoint;
+        this.accessKey = accessKey;
+        this.secretKey = secretKey;
+    }
+
+    @Bean
+    @Primary
+    public AmazonS3 amazonS3() {
+        AwsClientBuilder.EndpointConfiguration endpointConfiguration = new AwsClientBuilder
+                .EndpointConfiguration(endpoint, region);
+
+        AWSCredentials credentials = new BasicAWSCredentials(accessKey, secretKey);
+
+        AmazonS3 amazonS3 = AmazonS3ClientBuilder.standard()
+                .withEndpointConfiguration(endpointConfiguration)
+                .withCredentials(new AWSStaticCredentialsProvider(credentials))
+                .withPathStyleAccessEnabled(true)
+                .build();
+
+        if (!amazonS3.doesBucketExistV2(bucket)) {
+            amazonS3.createBucket(bucket);
+        }
+
+        return amazonS3;
+    }
+
+}

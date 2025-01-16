831d36451a04aa5b2157f10b3bce1f1c34a215e4
CodeJesterND <dev.hyoseung@gmail.com>
2025-01-16 13:09:07 +0900

    ♻️ 코드 리팩토링 : ResizedImage 클래스에 파라미터 검증 로직 추가 및 예외 처리 구현 - `CustomImageExceptionHandler` 및 `ExceptionResponseCode` import 추가 - `ResizedImage` 생성자에 파라미터 유효성 검사 로직 추가   - `inputStream`, `imageFormat`이 `null`인지 확인   - `size`가 0 이하인지 확인 - 유효하지 않은 파라미터일 경우 `CustomImageExceptionHandler` 예외 발생
    Ref : #18

--- src/main/java/com/realworld/infrastructure/image/ResizedImage.java
+++ src/main/java/com/realworld/infrastructure/image/ResizedImage.java
@@ -1,8 +1,11 @@
 package com.realworld.infrastructure.image;
 
+import com.realworld.common.exception.CustomImageExceptionHandler;
+import com.realworld.common.response.code.ExceptionResponseCode;
 import lombok.Getter;
 
 import java.io.InputStream;
+import java.util.Objects;
 
 @Getter
 public class ResizedImage implements AutoCloseable {
@@ -15,6 +18,13 @@ public class ResizedImage implements AutoCloseable {
         this.inputStream = inputStream;
         this.imageFormat = imageFormat;
         this.size = size;
+        notNullParameters(inputStream, imageFormat, size);
+    }
+
+    private void notNullParameters(InputStream inputStream, String imageFormat, long size) {
+        if (Objects.isNull(inputStream) || Objects.isNull(imageFormat) || size <= 0) {
+            throw new CustomImageExceptionHandler(ExceptionResponseCode.FILE_IMAGE_PROCESSING_ERROR);
+        }
     }
 
     public static ResizedImage of(InputStream inputStream, String imageFormat, long size) {

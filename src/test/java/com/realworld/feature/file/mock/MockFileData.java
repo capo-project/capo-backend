bbd68614fb7e2b515c46193b90a9a06bcadaf8b4
CodeJesterND <dev.hyoseung@gmail.com>
2025-01-20 12:46:21 +0900

    ♻️ 코드 리팩토링: `MockFileData` 클래스에서 `FILE_FORM_NAME` 상수 제거 및 실제 파일 사이즈 사용
    Ref: #18

--- src/test/java/com/realworld/feature/file/mock/MockFileData.java
+++ src/test/java/com/realworld/feature/file/mock/MockFileData.java
@@ -4,32 +4,33 @@ import com.realworld.feature.file.entity.FileDetails;
 import com.realworld.feature.file.entity.FileMetaData;
 import org.springframework.mock.web.MockMultipartFile;
 
-public class MockFileData {
+import java.io.File;
 
-    private static final String FILE_FORM_NAME = "file";
+public class MockFileData {
 
     public static final String FILE_NAME = "test-image.jpeg";
     public static final String FILE_CONTENT_TYPE = "image/jpeg";
     public static final long FILE_SIZE = 10 * 1024 * 1024;
 
+    public static final File testFile = new File("src/test/resources/image/test-image.jpeg");
+    public static final FileDetails fileDetails = FileDetails.of(
+            FILE_NAME,
+            FILE_CONTENT_TYPE,
+            testFile.length()
+    );
+
+    public static FileMetaData create(String targetDir) {
+        return FileMetaData.builder()
+                .directory(targetDir)
+                .details(fileDetails)
+                .build();
+    }
+
     public static MockMultipartFile multipartFile = new MockMultipartFile(
-            FILE_FORM_NAME,
+            "file",
             FILE_NAME,
             FILE_CONTENT_TYPE,
             new byte[(int) FILE_SIZE]
     );
-
-    public static final FileDetails fileDetails = FileDetails.of(
-            FILE_NAME,
-            FILE_CONTENT_TYPE,
-            FILE_SIZE
-    );
-
-    public static FileMetaData create(String destinationDirectory) {
-        return FileMetaData.builder()
-                .directory(destinationDirectory)
-                .details(fileDetails)
-                .build();
-    }
 
 }
